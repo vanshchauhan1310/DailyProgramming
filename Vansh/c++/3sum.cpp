2e863fb66d674c6d61a490b89984fb44aa05e901
Vansh Raj Chauhan <Vanshchauhan1310@gmail.com>
2023-09-25 23:40:18 +0530

    Update 3sum.cpp

--- Vansh/c++/3sum.cpp
+++ Vansh/c++/3sum.cpp
@@ -16,15 +16,16 @@ cin>>arr[i];
 
 cout<<"\n";
 for(i=0;i<n;i++){
-for(j=i+1;j<n-1;j++){
-for(k=i+2;k<n-2;k++){
+for(j=i+1;j<n;j++){
+for(k=j+1;k<n;k++){
 
-if(arr[i]+arr[j]+arr[k]==0){
+if(arr[i]+arr[j]+arr[k]==6){
 cout<<i<<""<<j<<""<<k<<"\n";
 }
 
 }
 }
 }
+
 
 }
